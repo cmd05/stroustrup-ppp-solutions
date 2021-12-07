79de4a1f0562062356327f2b1054b9d45bc70432
Salil <salil.mittal05@gmail.com>
2021-12-07 14:05:25 +0530

    empty test file

--- test.cpp
+++ test.cpp
@@ -1,18 +1,8 @@
+#define _USE_MATH_DEFINES
+#include <math.h>
 #include <iostream>
-#include <vector>
 #include <string>
-#include <cmath>
-#include <iomanip>
 
 int main() {
-    long double sum = 0;
-    int terms = 9 * pow(10, 5);
-    long long int denom = 1;
-
-    for(int i = 0; i < terms; i++) {
-        sum += pow(-1, i) * ( 1 / double(denom) );
-        denom += 2;
-        std::cout << std::setprecision(15) << std::fixed << sum << "\n";
-        // std::cout << sum << "\n";
-    }
+    
 }
