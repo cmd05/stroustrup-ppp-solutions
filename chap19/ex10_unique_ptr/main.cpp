3320acd7f1c8de985ed88106a68c6c55e65f34d3
Salil <salil.mittal05@gmail.com>
2022-05-06 15:36:06 +0530

    add ex10

---
+++ chap19/ex10_unique_ptr/main.cpp
@@ -0,0 +1,28 @@
+// // 10. Implement a simple unique_ptr supporting only a constructor,
+// // destructor, –>, *, and release(). In particular, don’t try to implement
+// // an assignment or a copy constructor.
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "unique_ptr.h"
+
+struct X {
+	X(): name{"John Doe"}, age{0} {}
+	X(std::string n, int a): name{n}, age{a} {}
+
+	std::string name;
+	int age;
+};
+
+std::ostream& operator<<(std::ostream& os, const X& x) {
+	return os << "X: " << x.name;
+}
+
+
+int main() {
+	X x{"cmd", 18};
+	unique_ptr<X> u{new X{"cmd05", 17}};
+
+	std::cout << *u << "\t(age " << u->age << ")";
+}
