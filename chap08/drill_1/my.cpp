4368ab247c00e6a5ce8ef1b1505c19dff236d079
cmd05 <splendid.snippet670@slmail.me>
2023-11-13 20:16:05 +0530

    Explaining use of `using directive`

--- chap08/drill_1/my.cpp
+++ chap08/drill_1/my.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "my.h"
-using namespace std;
+using namespace std; // this directive is not leaked to use.cpp;
+                     // if the directive was added to my.h it would have leaked to use.cpp
 
 void print_foo(){
     cout << foo;
