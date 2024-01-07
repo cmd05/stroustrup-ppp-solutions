a12f477d02c154e4d9f7f061d6ab9febf553e500
cmd05 <splendid.snippet670@slmail.me>
2024-01-07 21:35:34 +0530

    fix stl_vector/main.cpp

--- chap19/stl_vector_implementation/main.cpp
+++ chap19/stl_vector_implementation/main.cpp
@@ -1,9 +1,13 @@
 #include "vector.h"
 #include "vector_base.h"
 #include <iostream>
+#include <vector>
 
 int main() {
+    vector v {1,2,3};
+    vector v2 {1,2,3,4,5};
 
-    std::cout << v.capacity() << "\n\n";
+    v = v2;
+
     for(int i = 0; i < v.size(); i++) std::cout << v[i] << '\n';
 }
