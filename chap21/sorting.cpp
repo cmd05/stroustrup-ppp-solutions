672dac3b55ff95bfe014acaa22b0f12cf8bcb205
cmd05 <splendid.snippet670@slmail.me>
2024-01-29 12:37:03 +0530

    fix chap21 binary search

--- chap21/sorting.cpp
+++ chap21/sorting.cpp
@@ -8,17 +8,23 @@ struct NoCase {
 	bool operator()(const std::string& x, std::string& y) { return true; }
 };
 
+// binary search for contiguous memory sequence
+// search in [begin, end)
+// user should not pass an empty sequence (dereferencing of empty array will occur)
 template<typename In, typename T>
-std::pair<In, bool> bin_s(In first, In last, T val) {
-	In p = first + (last - first) / 2;
-	if(p == first) return {p, false};
-	
-	if(*p < val)
-		return bin_s(p, last, val);
-	else if(*p > val)
-		return bin_s(first, p, val);
-	
-	return {p, true};
+std::pair<In, bool> bin(In begin, In end, T val) {
+    In middle = begin + (end - begin) / 2;
+
+    if(*middle == val)
+        return {middle, true}; // found
+    
+    if(begin == middle) // test this first to avoid infinite recursion
+        return {end, false}; // not found
+    
+    if(*middle < val)
+        return bin(middle, end, val); // search in right half
+    
+    return bin(begin, middle, val); // search in left half
 }
 
 int main() {
@@ -30,7 +36,7 @@ int main() {
 	std::copy(v.begin(), v.end(), it);
 	std::cout << '\n';
 
-	auto x = bin_s(v.begin(), v.end(), 67);
+	auto x = bin(v.begin(), v.end(), 67);
 
 	if(x.second) std::cout << *(x.first);
 	else std::cout << "not found";
